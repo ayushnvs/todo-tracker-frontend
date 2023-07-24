0719702fe5b0fdece849c556ceb3097dea32a654
Ayush Mishra <ayushvns@ayushvns>
2023-07-24 10:44:14 +0530

    modified

--- src/components/filterOptions.js
+++ src/components/filterOptions.js
@@ -4,84 +4,41 @@ class FilterOptions extends React.Component {
   constructor(props) {
     super(props)
 
-    this.filterBtn = React.createRef()
-    this.dropDownBox = React.createRef()
-    this.handleCategoryChange = this.handleCategoryChange.bind(this)
-    this.handlePriorityChange = this.handlePriorityChange.bind(this)
-    this.handleUserChange = this.handleUserChange.bind(this)
-    this.handleOptionChange = this.handleOptionChange.bind(this)
+    this.filterBtnNode = React.createRef()
+    this.dropDownBoxNode = React.createRef()
     this.showFilterDropdown = this.showFilterDropdown.bind(this)
-
-    this.state = {
-      name: '',
-      showCategory: '',
-      showPriority: '',
-      showUser: '',
-      showOption: ''
-    }
-  }
-
-  componentDidMount() {
-    this.setState({
-      showCategory: true,
-      showPriority: true,
-      showUser: true,
-      showOption: true
-    })
+    
   }
   
   showFilterDropdown() {
-      let dropDownBoxNode = this.dropDownBox.current
-      let dropdownClasses = dropDownBoxNode.getAttribute('class')
-      if (dropdownClasses.includes(' d-none')) {
-        dropDownBoxNode.setAttribute('class', dropdownClasses.replace(' d-none', ''))
-      }
-      else dropDownBoxNode.setAttribute('class', dropdownClasses + ' d-none')
-  }
-
-  handleCategoryChange() {
-    this.setState({
-      showCategory: !this.state.showCategory
-    })
-  }
-
-  handlePriorityChange() {
-    this.setState({
-      showPriority: !this.state.showPriority
-    })
-  }
-
-  handleUserChange() {
-    this.setState({
-      showUser: !this.state.showUser
-    })
-  }
-
-  handleOptionChange() {
-    this.setState({
-      showOption: !this.state.showOption
-    })
+    console.log(this.dropDownBoxNode)
+    let dropDownBoxNode = this.dropDownBoxNode.current
+    let dropdownClasses = dropDownBoxNode.getAttribute('class')
+    if (dropdownClasses.includes(' d-none')) {
+      dropDownBoxNode.setAttribute('class', dropdownClasses.replace(' d-none', ''))
+    }
+    else dropDownBoxNode.setAttribute('class', dropdownClasses + ' d-none')
   }
 
   render() {
     return (
       <>
-        <button ref={this.filterBtn} onClick={this.showFilterDropdown} type='button' className="btn btn-light filter border text-center">Column Filter</button>
-        <div ref={this.dropDownBox} className='task-filter shadow p-3 mt-1 rounded-3 d-none'>
+        <button ref={this.filterBtnNode} onClick={this.showFilterDropdown} type='button' className="btn btn-light filter border text-center">Column Filter</button>
+        <div ref={this.dropDownBoxNode} className='task-filter shadow p-3 mt-1 rounded-3 d-none'>
           <label className='d-block' htmlFor="category">
-            <input name='showCategory' checked={this.state.showCategory} onChange={this.handleCategoryChange} className='me-2' type="checkbox" />
+            <input name='showCategory' checked={this.props.filters[0].showCategory} onChange={this.props.handleCategoryChange} className='me-2' type="checkbox" />
             Show Category
           </label>
           <label className='d-block' htmlFor="priority">
-            <input name='showPriority' checked={this.state.showPriority} onChange={this.handlePriorityChange} className='me-2' type="checkbox" />
+            <input name='showPriority' checked={this.props.filters[0].showPriority} onChange={this.props.handlePriorityChange} className='me-2' type="checkbox" />
             Show Priority
           </label>
           <label className='d-block' htmlFor="user">
-            <input name='showUser' checked={this.state.showUser} onChange={this.handleUserChange} className='me-2' type="checkbox" />
+            <input name='showUser' checked={this.props.filters[0].showUser} onChange={this.props.handleUserChange} className='me-2' type="checkbox" />
             Show User
           </label>
           <label className='d-block' htmlFor="option">
-            <input name='showOption' checked={this.state.showOption} onChange={this.handleOptionChange} className='me-2' type="checkbox" />
+            <input name='showOption' checked={this.props.filters[0].showOption} onChange={this.props.handleOptionChange} className='me-2' type="checkbox" />
             Show Option
           </label>
         </div>
--- src/components/tasksList.js
+++ src/components/tasksList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import { Link } from 'react-router-dom'
 import TaskUpdates from './taskUpdates'
 import Dropdowns from './dropdowns'
@@ -10,19 +10,47 @@ export default class TasksList extends Component {
   constructor(props) {
     super(props)
 
+    this.statusHeadNode = React.createRef()
+    this.nameHeadNode = React.createRef()
+    this.userHeadNode = React.createRef()
+    this.optionHeadNode = React.createRef()
+    this.priorityHeadNode = React.createRef()
+    this.categoryHeadNode = React.createRef()
+    this.statusDataNode = React.createRef()
+    this.nameDataNode = React.createRef()
+    this.userDataNode = React.createRef()
+    this.optionDataNode = React.createRef()
+    this.priorityDataNode = React.createRef()
+    this.categoryDataNode = React.createRef()
     this.deleteTask = this.deleteTask.bind(this)
     this.updateData = this.updateData.bind(this)
+    this.changeDisplay = this.changeDisplay.bind(this)
+    this.handleCategoryChange = this.handleCategoryChange.bind(this)
+    this.handlePriorityChange = this.handlePriorityChange.bind(this)
+    this.handleUserChange = this.handleUserChange.bind(this)
+    this.handleOptionChange = this.handleOptionChange.bind(this)
 
     this.state = {
       tasks: [],
       categoryList: [],
       users: [],
       statusList: [],
-      priorityList: []
+      priorityList: [],
+      filters: []
     }
   }
 
   componentDidMount() {
+
+    this.setState({
+      filters: [{
+        showCategory: false,
+        showPriority: true,
+        showUser: false,
+        showOption: true
+      }]
+    })
+
     axios.get('http://localhost:5000/tasks')
       .then(res => {
         this.setState({
@@ -55,7 +83,7 @@ export default class TasksList extends Component {
     })
   }
 
-  async updateData(e, data, key) {
+  updateData(e, data, key) {
     let updatedTasks = this.state.tasks
     if (data === "updates") updatedTasks[key][data].push(e.currentTarget.value.trim())
     else updatedTasks[key][data] = e.currentTarget.value.trim()
@@ -73,6 +101,64 @@ export default class TasksList extends Component {
     })
   }
 
+  changeDisplay(node) {
+    let nodeClasses = node.getAttribute('class')
+    if (!nodeClasses) nodeClasses = ''
+    if (nodeClasses.includes(' d-none')) {
+      node.setAttribute('class', nodeClasses.replace(' d-none', ''))
+    }
+    else node.setAttribute('class', nodeClasses + ' d-none')
+  }
+
+  handleCategoryChange() {
+    let updatedFilter = this.state.filters
+    updatedFilter[0].showCategory = !updatedFilter[0].showCategory
+
+    console.log(this.categoryDataNode)
+    this.changeDisplay(this.categoryHeadNode.current)
+    this.changeDisplay(this.categoryDataNode.current)
+    
+    this.setState({
+      filters: updatedFilter
+    })
+  }
+
+  handlePriorityChange() {
+    let updatedFilter = this.state.filters
+    updatedFilter[0].showPriority = !updatedFilter[0].showPriority
+
+    this.changeDisplay(this.priorityHeadNode.current)
+    this.changeDisplay(this.priorityDataNode.current)
+
+    this.setState({
+      filters: updatedFilter
+    })
+  }
+
+  handleUserChange() {
+    let updatedFilter = this.state.filters
+    updatedFilter[0].showUser = !updatedFilter[0].showUser
+
+    this.changeDisplay(this.userHeadNode.current)
+    this.changeDisplay(this.userDataNode.current)
+
+    this.setState({
+      filters: updatedFilter
+    })
+  }
+
+  handleOptionChange() {
+    let updatedFilter = this.state.filters
+    updatedFilter[0].showOption = !updatedFilter[0].showOption
+
+    this.changeDisplay(this.optionHeadNode.current)
+    this.changeDisplay(this.optionDataNode.current)
+
+    this.setState({
+      filters: updatedFilter
+    })
+  }
+
   render() {
     return (
       <div className='mt-3'>
@@ -84,46 +170,52 @@ export default class TasksList extends Component {
             <Link className="btn btn-primary ms-5" to="/create">Add New Task</Link>
           </div>
           <div className="show-columns col-4">
-            <FilterOptions/>
+            {
+              this.state.filters.length > 0
+              ? 
+              <FilterOptions filters={this.state.filters} handleCategoryChange={this.handleCategoryChange} handleOptionChange={this.handleOptionChange} handlePriorityChange={this.handlePriorityChange} handleUserChange={this.handleUserChange}/>
+              : null
+            }
+            
           </div>
         </div>
         <div className="table-responsive">
           <table className="table mt-5">
             <thead className="thead-light">
               <tr>
-                <th>Status</th>
-                <th>Task Name</th>
-                <th>Updates</th>
-                <th>Category</th>
-                <th>Prority</th>
-                <th>User</th>
-                {/* <th>Options</th> */}
+                <th ref={this.statusHeadNode} >Status</th>
+                <th ref={this.nameHeadNode}>Task Name</th>
+                <th ref={this.updatesHeadNode}>Updates</th>
+                <th ref={this.categoryHeadNode}>Category</th>
+                <th ref={this.priorityHeadNode}>Prority</th>
+                <th ref={this.userHeadNode}>User</th>
+                <th ref={this.optionHeadNode}>Options</th>
               </tr>
             </thead>
             <tbody>
               {
                 this.state.tasks.map((currentTask, key) => {
                   return <tr key={currentTask._id}>
-                    <td>
+                    <td ref={this.statusDataNode}>
                       <Dropdowns name={"status"} dataList={this.state.statusList} task={currentTask} updateData={this.updateData} key={key} index={key} />
                     </td>
-                    <td>
+                    <td ref={this.nameDataNode}>
                       <div className="blockquote">{currentTask.name}</div>
                       {/* <div className="blockquote-footer">{currentTask.description}</div> */}
                     </td>
-                    <td>
+                    <td ref={this.updatesDataNode}>
                       <TaskUpdates currentTask={currentTask} key={key} updateData={this.updateData} taskKey={key} />
                     </td>
-                    <td>
+                    <td ref={this.categoryDataNode}>
                       <Dropdowns name={"category"} dataList={this.state.categoryList} task={currentTask} updateData={this.updateData} key={key} index={key} />
                     </td>
-                    <td>
+                    <td ref={this.priorityDataNode}>
                       <Dropdowns name={"priority"} dataList={this.state.priorityList} task={currentTask} updateData={this.updateData} key={key} index={key} />
                     </td>
-                    <td>
+                    <td ref={this.userDataNode}>
                       <Dropdowns name={"username"} dataList={this.state.users} task={currentTask} updateData={this.updateData} key={key} index={key} />
                     </td>
-                    {/* <td><Link to={"/edit/" + currentTask._id}>edit</Link> | <button className='delete-task link-button d-none' onClick={() => { this.deleteTask(currentTask._id) }}>delete</button></td> */}
+                    <td ref={this.optionDataNode}><Link to={"/edit/" + currentTask._id}>edit</Link> | <button className='delete-task link-button d-none' onClick={() => { this.deleteTask(currentTask._id) }}>delete</button></td>
                   </tr>
                 })
               }
